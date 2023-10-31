e3f67436f871f157db473171fe7da762f5e4c73c
Pablo Suárez Lapalma <84790504+PabloSuarezLapalma@users.noreply.github.com>
2023-10-31 15:59:48 -0300

    Semi-Responsive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -32,91 +32,127 @@ const Form = () => {
              <h1 className="text-3xl font-bold mt-5 text-center mb-10 pt-4 pb-4  text-white">Registrar Ingreso</h1>
          </div>
         <form onSubmit={handleSubmit} >
-            <div className='mb-3 mt-3'>
-                <label className='left-0 text-lg'> 
-                    ID Cliente:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={idCliente} onChange={(e) => setIDCliente(e.target.value)}  />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Código BWS:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={codigoBWS} onChange={(e) => setCodigoBWS(e.target.value)}  />
-                </label>
+            <div className='flex flex-wrap'>
+                <div className='w-full sm:w-auto'>
+                    <label className='left-0 text-lg'> 
+                        ID Cliente:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={idCliente} onChange={(e) => setIDCliente(e.target.value)}  />
+                    </label>
+                </div>
+                <div className='w-full sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Código BWS:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={codigoBWS} onChange={(e) => setCodigoBWS(e.target.value)}  />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Fecha:
-                    <input type="date" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={fecha} onChange={(e) => setFecha(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Hora aproximada de ingreso:
-                    <input type="time" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={hora} onChange={(e) => setHora(e.target.value)} />
-                </label>
+            <div className='flex flex-wrap'>
+                <div className='mb-3 mt-3 w-full sm:w-auto'>
+                    <label className='text-lg'>
+                        Fecha:
+                        <input type="date" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={fecha} onChange={(e) => setFecha(e.target.value)} />
+                    </label>
+                </div>
+                <div className='mb-3 mt-3 w-full sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Hora aproximada de ingreso:
+                        <input type="time" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={hora} onChange={(e) => setHora(e.target.value)} />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Nro Remito:
-                    <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={nroRemito} onChange={(e) => setNroRemito(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Transporte:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={transporte} onChange={(e) => setTransporte(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Tipo (de Transporte):
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={tipoTransporte} onChange={(e) => setTipoTransporte(e.target.value)} />
-                </label>
+            <div className='flex flex-wrap'>
+                <div className=' w-full sm:w-auto'>
+                    <label className='text-lg'>
+                        Nro Remito:
+                        <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={nroRemito} onChange={(e) => setNroRemito(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Transporte:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={transporte} onChange={(e) => setTransporte(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Tipo:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={tipoTransporte} onChange={(e) => setTipoTransporte(e.target.value)} />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Chofer:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={chofer} onChange={(e) => setChofer(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Chasis:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={chasis} onChange={(e) => setChasis(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Acoplado:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={acoplado} onChange={(e) => setAcoplado(e.target.value)} />
-                </label>
+            <div className='mb-3 mt-3 flex flex-wrap'>
+                <div className=' w-full  sm:w-auto'>
+                    <label className='text-lg'>
+                        Chofer:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={chofer} onChange={(e) => setChofer(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Chasis:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={chasis} onChange={(e) => setChasis(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Acoplado:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={acoplado} onChange={(e) => setAcoplado(e.target.value)} />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Descripción:
-                    <textarea type="text" className='ml-2 rounded-md pl-2 w-1/3 h-8   align-middle bg-gray-500 text-white border-2 border-black' value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Cantidad:
-                    <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black'  value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
-                </label>
+            <div className='mb-3 mt-3 flex flex-wrap'>
+                <div className='w-full sm:w-auto'>
+                    <label className='text-lg'>
+                        Descripción:
+                        <textarea type="text" className='ml-2 rounded-md pl-2  h-8 align-middle bg-gray-500 text-white border-2 border-black' value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Cantidad:
+                        <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black'  value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Posición:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' pattern="[A-Za-z]" maxLength={1} value={posicion} onChange={(e) => setPosicion(e.target.value.toUpperCase())} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Sector:
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' pattern="[A-Za-z]" maxLength={1} value={sector} onChange={(e) => setSector(e.target.value.toUpperCase())} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Altura:
-                    <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={altura} onChange={(e) => setAltura(e.target.value)} />
-                </label>
+            <div className='mb-3 mt-3 flex flex-wrap'>
+                <div className='w-full  sm:w-auto'>
+                    <label className='text-lg'>
+                        Posición:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' pattern="[A-Za-z]" maxLength={1} value={posicion} onChange={(e) => setPosicion(e.target.value.toUpperCase())} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Sector:
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' pattern="[A-Za-z]" maxLength={1} value={sector} onChange={(e) => setSector(e.target.value.toUpperCase())} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Altura:
+                        <input type="number" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={altura} onChange={(e) => setAltura(e.target.value)} />
+                    </label>
+                </div>
             </div>
-            <div className='mb-3 mt-3'>
-                <label className='text-lg'>
-                    Ancho(cm):
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={ancho} onChange={(e) => setAncho(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Largo(cm):
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={largo} onChange={(e) => setLargo(e.target.value)} />
-                </label>
-                <label className='ml-10 text-lg'>
-                    Alto(cm):
-                    <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={alto} onChange={(e) => setAlto(e.target.value)} />
-                </label>
+            <div className='mb-3 mt-3 flex flex-wrap'>
+                <div className='w-full  sm:w-auto'>
+                    <label className='text-lg'>
+                        Ancho(cm):
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={ancho} onChange={(e) => setAncho(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Largo(cm):
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={largo} onChange={(e) => setLargo(e.target.value)} />
+                    </label>
+                </div>
+                <div className='w-full  sm:w-auto'>
+                    <label className='ml-10 text-lg'>
+                        Alto(cm):
+                        <input type="text" className='ml-2 rounded-md pl-2 bg-gray-500 text-white border-2 border-black' value={alto} onChange={(e) => setAlto(e.target.value)} />
+                    </label>
+                </div>
             </div>
             <button type="submit" className='bg-red-600 font-semibold  rounded-full text-white'>Guardar</button>
         </form>
