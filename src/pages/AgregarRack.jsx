97e399137e6064b184036a29cc1e22e4eef4248a
Lucas Tonelotto <lucasfrantone@gmail.com>
2023-11-25 15:57:08 -0300

    Ahora el AgregarRack funciona correctamente, además se lo implementó provisoriamente en el /home y se hicieron comentarios que clarifican el código en ámbos casos

--- src/pages/AgregarRack.jsx
+++ src/pages/AgregarRack.jsx
@@ -7,14 +7,24 @@ const AgregarRack = ({racks,setRacks}) => {
         e.preventDefault();
 
         //Objeto rack        
-        const objetoRack = [altura][posicion];
-        
-        console.log(objetoRack.length);
-        console.log(objetoRack[0].length);
-        setRacks([...racks, objetoRack]);
+        const objetoRack = [];
 
-       // setAltura(1);
-       // setPosicion(1);
+        for (let i = 0; i < altura; i++) {
+            objetoRack[i] = [];
+            for (let j = 0; j < posicion; j++) {
+                objetoRack[i][j] = null;
+            }
+        } 
+        
+        //Agrego el rack al array de racks
+        setRacks([...racks, objetoRack]);
+        
+        //Reseteo los valores de los inputs
+        setAltura(1);
+        setPosicion(1);
+
+
+
 
     };
 
@@ -23,15 +33,15 @@ const AgregarRack = ({racks,setRacks}) => {
     
 
     return (
-        <div className=' max-w-7xl mx-auto px-8 p-8 text-center'>
+        <div className=' max-w-7xl mx-auto lg:px-72 md:px-36 p-8 md:w-full text-left'>
         <div className="bg-white pt-4 pb-4 pl-2 pr-2 rounded-lg shadow-md text-black sm:w-fit md:w-fit lg:w-fit xl:w-fit">
             <div className="shadow-md bg-red-500 mx-auto rounded-md sm:w-11/12 md:w-3/4 lg:w-1/2 xl:w-11/12">
-                <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-bold mt-5 text-center mb-10 pt-4 pb-4 text-white ">Registrar Egreso</h1>
+                <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-6xl font-bold mt-5 text-center mb-10 pt-4 pb-4 text-white ">Registrar Nuevo Rack</h1>
             </div>
 
-        <form className= 'py-10 sm:ml-10 md:ml-10 lg:ml-10 xl:px-30 2xl:ml-50 sm:px-5' onSubmit={handleSubmit} >
+        <form className= 'sm:ml-1 lg:ml-16 md:ml-14 sm:px-5' onSubmit={handleSubmit} >
             <div className='flex flex-wrap '>
-            <div className=' w-full md:mx-2 lg:mx-2 xl:mx-2 2xl:mx-2 sm:mx-2 xs:mx-2 sm:w-auto py-5 '>
+            <div className=' w-full md:mx-2 lg:mx-2 xl:mx-2 2xl:mx-2 sm:mx-2 xs:mx-2 sm:w-auto px-10 '>
                     <label htmlFor='posicion' className='block text-md font-medium leading-6 text-gray-900'> 
                         Cantidad de columnas
                     </label>
@@ -46,15 +56,15 @@ const AgregarRack = ({racks,setRacks}) => {
                             onChange={(e) => setPosicion(e.target.value)}  
                         />
                 </div>
-                <div className=' w-full md:mx-2 lg:mx-2 xl:mx-2 2xl:mx-2 sm:mx-2 xs:mx-2 sm:w-auto py-5'>
-                    <label htmlFor='altura' className='block text-md font-medium leading-6 text-gray-900'>
+                <div className=' w-full md:mx-2 lg:mx-2 xl:mx-2 2xl:mx-2 sm:mx-2 xs:mx-2 sm:w-auto px-10 '>
+                <label htmlFor='altura' className='block text-md font-medium leading-6 text-gray-900'> 
                         Cantidad de filas
                     </label>
                         <input
                             id='altura'
                             name="altura" 
                             type="number" 
-                            className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6" 
+                            className="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
                             value={altura}
                             required
                             placeholder='Cantidad de filas' 
@@ -66,7 +76,7 @@ const AgregarRack = ({racks,setRacks}) => {
                 type="submit"
                 id="agregarRack"
                 value="Registrar Rack" 
-                className='bg-red-500 font-semibold mt-5 rounded-md text-white justify-center px-10 py-2 text-lg leading-6 shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500'
+                className='bg-red-500 sm:ml-40 font-semibold mt-5 rounded-md text-white justify-center px-8  py-2 text-lg leading-6 shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500'
             />
             </div>
         </form>
