844a04b41373f543a6703bdae755e49f4cbf6335
Pablo Suárez Lapalma <suarezp@o365frcuutneduar.onmicrosoft.com>
2023-12-04 15:59:53 -0300

    Agregado de js clientes y movimientos modificado

--- src/scripts/movimientos.js
+++ src/scripts/movimientos.js
@@ -4,34 +4,31 @@ const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzd
 const SUPABASE_URL = 'https://ewathdqpvxumtxwrmwgw.supabase.co'
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY  )
 
-export async function obtenerMovimientos() {
+export  async function obtenerMovimientos() {
     try {
         let { data: Movimientos, error } = await supabase
             .from('Movimientos')
             .select('*');
-
         if (error) {
             throw new Error(error.message);
         }
-
-        let listaDeMovimientos = Movimientos.map(item => {
-            return item;});
+        let listaDeMovimientos = Movimientos.map(item => {return item;});
         return listaDeMovimientos;
     } catch (error) {
         console.error(error);
     }
 }
 //?Descomentar para ver por consola el listado de movimientos
-//const listaDeMovimientos = getMovimientos();
+//const listaDeMovimientos = obtenerMovimientos();
 //console.log(listaDeMovimientos);
 
-export async function insertarMovimiento(codigo, fechaHora, nroRemito, estado, responsable, transporte, chasis, chofer, acoplado, costo, idMercaderia){
+export  async function insertarMovimiento(codigo, fecha,hora, nroRemito, estado, responsable, transporte, chasis, chofer, acoplado, costo, idMercaderia,codigoCliente){
     let code=0;
     try {
         const { data, error } = await supabase
             .from('Movimientos')
             .insert([
-                { codigoBWS: codigo, fechaHora: fechaHora, nroRemito: nroRemito, estado: estado, nombreResponsable: responsable, descripTransporte: transporte, chasis: chasis, chofer: chofer, acoplado: acoplado, costo: costo, idMercaderia: idMercaderia},
+                { codigoBWS: codigo, fecha:fecha,hora:hora, nroRemito: nroRemito, estado: estado, nombreResponsable: responsable, descripTransporte: transporte, chasis: chasis, chofer: chofer, acoplado: acoplado, costo: costo, idMercaderia: idMercaderia,codigoCliente:codigoCliente},
             ])
             .select()
         if (error) {
@@ -45,7 +42,7 @@ export async function insertarMovimiento(codigo, fechaHora, nroRemito, estado, r
     return code
 }
 
-export async function borrarMovimiento(codigoBWS){
+export  async function borrarMovimiento(codigoBWS){
     let code=0;
     try{
         const { error } = await supabase
@@ -60,28 +57,62 @@ export async function borrarMovimiento(codigoBWS){
         code=1;
         console.error(error);
     }
+    return code
 }
 
-export async function filtrarMovimiento(codigoBWS){
-    let code=0;
+//*Esto debe usarse en conjunto con la funcion obtenerCodigoCliente de clientes.js ya que se obtiene el codigo a partir del nombre y luego se busca el movimiento con ese codigo
+export  async function filtrarMovimiento(codigoCliente){
     try{
         let { data: Movimientos, error } = await supabase
         .from('Movimientos')
         .select("*")
-        .ilike('codigoBWS', codigoBWS)
+        .ilike('codigoCliente', codigoCliente)
         if (error) {
-            code=1;
             throw new Error(error.message);}   
-        let listaFiltrada = Movimientos.map(item => {
-            return item;});
+        let listaFiltrada = Movimientos.map(item => {return item;});
         return listaFiltrada; 
     }
     catch (error){
-       code=1;
        console.log(error)
 }
 }
 
+export async function filtrarMovimientosEntreFechas(fechaInicio, fechaFin){
+    try{
+        let { data: Movimientos, error } = await supabase
+        .from('Movimientos')
+        .select("*")
+        .gte('fecha', fechaInicio)
+        .lte('fecha', fechaFin)
+        if (error) {
+            throw new Error(error.message);}   
+        let listaFiltrada = Movimientos.map(item => {return item;});
+        return listaFiltrada; 
+    }
+    catch (error){
+           console.log(error)
+    }
+}
+
+/*
+filtrarMovimientosEntreFechas("2021-10-01","2024-10-31").then(resultado=> {
+    if (Array.isArray(resultado)) {
+        // Itera sobre cada elemento del array
+        resultado.forEach(elemento => {
+            console.log("Código BWS:", elemento.codigoBWS);
+            console.log("Fecha y Hora:", elemento.fechaHora);
+            console.log("Responsable:", elemento.nombreResponsable);
+            console.log("idMercaderia:", elemento.idMercaderia);
+            console.log("Descripcion:", elemento.descripcionTransporte);
+            console.log("Codigo del Cliente: ", elemento.codigoCliente);
+            // ... y así sucesivamente para otros campos
+        });
+    } else {
+        console.error("El resultado no es un array.");
+    }  
+    }
+)
+*/
 //deleteMovimiento("PBC-BJ1-3212");
 
 //?Si se descomenta esto para probar por consola, cambiar el valor del codigoBWS, porque sino no deja hacer el insert porque existen claves primarias duplicadas
@@ -89,7 +120,7 @@ export async function filtrarMovimiento(codigoBWS){
 
 //let filtrada=filtrarMovimiento("PBC-BJ1-3212")
 
-//*Esta es la forma de acceder a los datos de la función, como es asíncrono siempre el resultado es una Promise, por lo que se debe acceder de la siguiente manera para poder manipular los datos
+//!Esta es la forma de acceder a los datos de la función, como es asíncrono siempre el resultado es una Promise, por lo que se debe acceder de la siguiente manera para poder manipular los datos
 /*
 filtrarMovimiento("PBC-BJ1-3212").then(resultado=> {
     if (Array.isArray(resultado)) {
@@ -100,6 +131,7 @@ filtrarMovimiento("PBC-BJ1-3212").then(resultado=> {
             console.log("Responsable:", elemento.nombreResponsable);
             console.log("idMercaderia:", elemento.idMercaderia);
             console.log("Descripcion:", elemento.descripcionTransporte);
+            console.log("Codigo del Cliente: ", elemento.codigoCliente);
             // ... y así sucesivamente para otros campos
         });
     } else {
